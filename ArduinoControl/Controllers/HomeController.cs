bd1712d18e3542ed8a8789fa8756ee938857b488
upretymadan45@gmail.com <upretymadan45@gmail.com>
2020-03-30 14:06:03 +0545

    project upload

---
+++ ArduinoControl/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ArduinoControl.Models;
+using ArduinoControl.library;
+
+namespace ArduinoControl.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private SerialPortConnector _serialPortConnector;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _serialPortConnector = new SerialPortConnector();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Send(string command,string roomNumber)
+        {
+            try
+            {
+                _serialPortConnector.Send(command+roomNumber);
+                return Ok("sent");
+            }
+            catch (Exception)
+            {
+                return BadRequest("failed");
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ArduinoControl/library/SerialPortConnector.cs
@@ -0,0 +1,19 @@
+﻿using System.IO.Ports;
+
+namespace ArduinoControl.library
+{
+    public class SerialPortConnector
+    {
+        private readonly int _baudRate = 9600;
+        private readonly string _portName = "COM3";
+
+        public void Send(string command)
+        {
+            using(var serialPort = new SerialPort(_portName, _baudRate))
+            {
+                serialPort.Open();
+                serialPort.Write(command);
+            }
+        }
+    }
+}
